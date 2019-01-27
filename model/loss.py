ce89fb4e4bb976bed035055ecf25a9e9c36bbbcb
andi611 <tingweiandyliu@gmail.com>
2019-01-28 00:00:25 +0800

    fix bug

--- model/loss.py
+++ model/loss.py
@@ -30,7 +30,7 @@ class TacotronLoss(nn.Module):
 		linear_outputs, linear = model_output[1], targets[1]
 		gate_outputs, gate = model_output[2], targets[2]
 
-		mel_loss = criterions[0](mel_outputs, mel)
+		mel_loss = cself.criterion(mel_outputs, mel)
 		n_priority_freq = int(3000 / (self.sample_rate * 0.5) * self.linear_dim)
 		linear_loss = 0.5 * self.criterion(linear_outputs, linear) + 0.5 * self.criterion(linear_outputs[:, :, :n_priority_freq], linear[:, :, :n_priority_freq])
 		gate_loss = self.criterion_gate(gate_outputs, gate)
